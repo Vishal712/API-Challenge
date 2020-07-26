c7436c2df49f27ccd2b9c6b6ac7c54a1818ce54e
unknown <vipvishu712@gmail.com>
2020-07-25 21:54:51 -0700

    First Commit to submit

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "Please use your own keys"
+
+# Google API Key
+g_key = "Please use your own keys"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "Please use your own"
+
+# Google API Key
+g_key = "Please use your own"
